5ed9d40ab9fc7220f37fe22f40a1f3140971ec66
Jana Ahurtsova <byjankaaa@gmail.com>
2025-02-10 15:35:56 +0300

    feat: add form

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,10 +1,78 @@
+"use client"
+import { useForm } from "react-hook-form"
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from 'zod'
+import Link from "next/link"
+
 import { AuthCard } from "./auth-card"
+import { loginSchema } from "@/types/login-schema";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
 
 export const LoginForm = () => {
+  const form = useForm({ 
+    resolver: zodResolver(loginSchema), 
+    defaultValues: {
+      email: '',
+      password: ''
+    },
+    mode: "onChange",
+  });
+
+const onSubmit = (data: z.infer<typeof loginSchema>) => {
+  console.log(data)
+ }
   return <AuthCard cardTitle="Welcome back!"
       backButtonHref="/auth/register"
       backButtonLabel="Create a new account"
       showSocials>
-        <div></div>
+        <div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <div>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="email@example.com" {...field} type="email" autoComplete="email" />
+                    </FormControl>
+                    <FormDescription />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input placeholder="********" {...field} type="password" autoComplete="current-password" />
+                    </FormControl>
+                    <FormDescription />
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button size="sm" variant="link" asChild>
+                <Link href="/auth/reset">
+                  Forgot your password
+                </Link>
+              </Button>
+            </div>
+            <Button
+              type="submit"
+              className="w-full my-2"
+            >
+              Sign In
+            </Button>
+          </form>
+        </Form>
+        </div>
       </AuthCard>
 }
--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,9 +1,11 @@
+import * as React from "react"
+
 import { cn } from "@/lib/utils"
 
-type Props = React.HTMLAttributes<HTMLDivElement> & {
-  ref?: React.Ref<HTMLDivElement>
-}
-const Card = ({className, ref, ...props}: Props) => (
+const Card = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
   <div
     ref={ref}
     className={cn(
@@ -12,20 +14,26 @@ const Card = ({className, ref, ...props}: Props) => (
     )}
     {...props}
   />
-)
+))
 Card.displayName = "Card"
 
-const CardHeader = ({ className, ref, ...props }: Props) => (
+const CardHeader = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
   <div
     ref={ref}
     className={cn("flex flex-col space-y-1.5 p-6", className)}
     {...props}
   />
-)
+))
 CardHeader.displayName = "CardHeader"
 
-const CardTitle = ({ className, ref, ...props }: Props) => (
-  <h3
+const CardTitle = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
     ref={ref}
     className={cn(
       "text-2xl font-semibold leading-none tracking-tight",
@@ -33,29 +41,39 @@ const CardTitle = ({ className, ref, ...props }: Props) => (
     )}
     {...props}
   />
-)
+))
 CardTitle.displayName = "CardTitle"
 
-const CardDescription = ({ className, ref, ...props }: Props) => (
-  <p
+const CardDescription = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
     ref={ref}
     className={cn("text-sm text-muted-foreground", className)}
     {...props}
   />
-)
+))
 CardDescription.displayName = "CardDescription"
 
-const CardContent = ({ className, ref, ...props }: Props) => (
+const CardContent = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
   <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
-)
+))
 CardContent.displayName = "CardContent"
 
-const CardFooter = ({ className, ref, ...props }: Props) => (
+const CardFooter = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
   <div
     ref={ref}
     className={cn("flex items-center p-6 pt-0", className)}
     {...props}
   />
-)
+))
 CardFooter.displayName = "CardFooter"
+
 export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
